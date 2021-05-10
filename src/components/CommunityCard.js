2e7150a92cb1816bd2a92964747182ba6d83b124
healing99 <nicealice99@gmail.com>
2021-05-10 21:25:28 +0900

    update: modify CommunityCard css

--- src/components/CommunityCard.js
+++ src/components/CommunityCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Styled from 'styled-components';
 
 const CardWrapper = Styled.div`
-  .community-card{
+  .card{
     width: 350px;
     height: 260px;
     background-color: var(--theme-color);
@@ -13,13 +13,13 @@ const CardWrapper = Styled.div`
     align-items: center;
   }
 
-  .community-card__img{
+  .card__img {
     width: 150px;
     height: 160px;
     background-color: lightgrey;
   }
 
-  .community-card__content{
+  .card__content{
     display: flex;
     flex-direction: column;
     font-size: 17px;
@@ -29,21 +29,21 @@ const CardWrapper = Styled.div`
     position: relative;
   }
 
-  .community-card__content > p{
+  .card__content > p{
     margin-top: 5px;
   }
 
-  .community-card__content_date{
+  .card__content_date{
     font-size: 14px;
     margin-bottom: 12px;
   }
 
-  .community-card__content_category,
-  .community-card__content_title{
+  .card__content_category,
+  .card__content_title{
     font-weight: bold;
   }
 
-  .community-card button{
+  .card button{
     border: none;
     padding: 8px;
     width: 90px;
@@ -53,24 +53,21 @@ const CardWrapper = Styled.div`
     position: absolute;
     right: 0;
     bottom: 0;
-
-    &:hover{
-      cursor: pointer;
-    }
+    cursor: pointer;
   }
 `;
 
 const CommunityCard = () => {
   return (
     <CardWrapper>
-      <div className="community-card">
-        <div className="community-card__img"></div>
-        <div className="community-card__content">
-          <p className="community-card__content_date">작성일자</p>
-          <p className="community-card__content_category">[카테고리]</p>
-          <p className="community-card__content_title"> 도서명</p>
-          <p className="community-card__content_author">저자</p>
-          <p className="community-card__content_publisher">출판사, 출판일</p>
+      <div className="card">
+        <div className="card__img"></div>
+        <div className="card__content">
+          <p className="card__content_date">작성일자</p>
+          <p className="card__content_category">[카테고리]</p>
+          <p className="card__content_title"> 도서명</p>
+          <p className="card__content_author">저자</p>
+          <p className="card__content_publisher">출판사, 출판일</p>
           <button>게시글보기</button>
         </div>
       </div>
