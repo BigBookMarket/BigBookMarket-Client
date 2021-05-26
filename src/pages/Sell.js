3480c11c2970ee527089b295437ce7feaf2b918d
healing99 <nicealice99@gmail.com>
2021-05-26 17:01:02 +0900

    fix(Sell): add userId to postData

--- src/pages/Sell.js
+++ src/pages/Sell.js
@@ -7,6 +7,7 @@ import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 import axios from "axios";
 import SearchDropdown from "../components/SearchDropdown";
+import Navbar from "../components/Navbar";
 
 const SellWrapper = Styled.div`
   display: flex;
@@ -206,7 +207,7 @@ const Sell = () => {
       detail: inputs.detail,
       method: inputs.method,
       price: parseInt(inputs.sellPrice),
-      sellerId: 1,
+      id: localStorage.getItem("userId"),
     };
     console.log(postData);
     (async () => {
@@ -248,131 +249,134 @@ const Sell = () => {
   }, [options, selectedBook]);
 
   return (
-    <SellWrapper>
-      <div className="page-title">판매글 작성하기</div>
-      <form onSubmit={handleSubmit}>
-        <div className="product-form">
-          <img className="product-form__img" src={coverImage} alt="" />
-          <div className="product-form__info">
-            <div className="info__search">
-              <SearchDropdown
-                handleSearchInput={handleSearchInput}
-                searchInput={searchInput}
-                options={options}
-                onChange={(val) => setSearchInput(val)}
-                setSelectedBook={setSelectedBook}
-              />
-              <button onClick={handleSearchButton}>검색</button>
-            </div>
+    <>
+      <Navbar />
+      <SellWrapper>
+        <div className="page-title">판매글 작성하기</div>
+        <form onSubmit={handleSubmit}>
+          <div className="product-form">
+            <img className="product-form__img" src={coverImage} alt="" />
+            <div className="product-form__info">
+              <div className="info__search">
+                <SearchDropdown
+                  handleSearchInput={handleSearchInput}
+                  searchInput={searchInput}
+                  options={options}
+                  onChange={(val) => setSearchInput(val)}
+                  setSelectedBook={setSelectedBook}
+                />
+                <button onClick={handleSearchButton}>검색</button>
+              </div>
 
-            <input
-              onChange={handleInputChange}
-              name="category"
-              value={category}
-              className="info__category"
-              placeholder="카테고리"
-              readOnly
-            />
-            <input
-              name="title"
-              onChange={handleInputChange}
-              value={title}
-              className="info__title"
-              placeholder="도서명"
-              readOnly
-            />
-            <input
-              onChange={handleInputChange}
-              value={standardPrice}
-              name="standardPrice"
-              className="info__price"
-              placeholder="정가정보"
-              readOnly
-            />
-            <input
-              name="author"
-              onChange={handleInputChange}
-              value={author}
-              className="info__author"
-              placeholder="저자"
-              readOnly
-            />
-            <input
-              onChange={handleInputChange}
-              value={publisher}
-              name="publisher"
-              className="info__publisher"
-              placeholder="출판사/ 출판일"
-              readOnly
-            />
-          </div>
-        </div>
-        <div className="deal-form">
-          <div className="deal-form__info">
-            <input
-              type="number"
-              onChange={handleInputChange}
-              value={sellPrice}
-              className="info__sellPrice"
-              name="sellPrice"
-              placeholder="판매가 (원)"
-            />
-            <FormControl variant="outlined" className={classes.formControl}>
-              <InputLabel id="demo-simple-select-outlined-label">
-                거래방법
-              </InputLabel>
-              <Select
-                labelId="demo-simple-select-outlined-label"
-                id="demo-simple-select-outlined"
-                name="method"
-                value={method}
+              <input
                 onChange={handleInputChange}
-                label="거래방법"
-              >
-                <MenuItem value="DELIVERY">
-                  <em>택배</em>
-                </MenuItem>
-                <MenuItem value="DIRECT">
-                  <em>직거래</em>
-                </MenuItem>
-                <MenuItem value="BOTH">
-                  <em>둘다가능</em>
-                </MenuItem>
-              </Select>
-            </FormControl>
-            <FormControl variant="outlined" className={classes.formControl}>
-              <InputLabel id="demo-simple-select-outlined-label">
-                거래상태
-              </InputLabel>
-              <Select
-                labelId="demo-simple-select-outlined-label"
-                id="demo-simple-select-outlined"
-                name="status"
-                value={status}
+                name="category"
+                value={category}
+                className="info__category"
+                placeholder="카테고리"
+                readOnly
+              />
+              <input
+                name="title"
                 onChange={handleInputChange}
-                label="거래상태"
-              >
-                <MenuItem value="SALE">
-                  <em>판매중</em>
-                </MenuItem>
-              </Select>
-            </FormControl>
+                value={title}
+                className="info__title"
+                placeholder="도서명"
+                readOnly
+              />
+              <input
+                onChange={handleInputChange}
+                value={standardPrice}
+                name="standardPrice"
+                className="info__price"
+                placeholder="정가정보"
+                readOnly
+              />
+              <input
+                name="author"
+                onChange={handleInputChange}
+                value={author}
+                className="info__author"
+                placeholder="저자"
+                readOnly
+              />
+              <input
+                onChange={handleInputChange}
+                value={publisher}
+                name="publisher"
+                className="info__publisher"
+                placeholder="출판사/ 출판일"
+                readOnly
+              />
+            </div>
           </div>
-          <div className="deal-form__detail">
-            <p>상품 상세 내용을 입력해주세요. 상품 상태도 기입해주세요</p>
-            <textarea
-              name="detail"
-              value={detail}
-              onChange={handleInputChange}
-              placeholder="설명"
-            />
+          <div className="deal-form">
+            <div className="deal-form__info">
+              <input
+                type="number"
+                onChange={handleInputChange}
+                value={sellPrice}
+                className="info__sellPrice"
+                name="sellPrice"
+                placeholder="판매가 (원)"
+              />
+              <FormControl variant="outlined" className={classes.formControl}>
+                <InputLabel id="demo-simple-select-outlined-label">
+                  거래방법
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-outlined-label"
+                  id="demo-simple-select-outlined"
+                  name="method"
+                  value={method}
+                  onChange={handleInputChange}
+                  label="거래방법"
+                >
+                  <MenuItem value="DELIVERY">
+                    <em>택배</em>
+                  </MenuItem>
+                  <MenuItem value="DIRECT">
+                    <em>직거래</em>
+                  </MenuItem>
+                  <MenuItem value="BOTH">
+                    <em>둘다가능</em>
+                  </MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl variant="outlined" className={classes.formControl}>
+                <InputLabel id="demo-simple-select-outlined-label">
+                  거래상태
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-outlined-label"
+                  id="demo-simple-select-outlined"
+                  name="status"
+                  value={status}
+                  onChange={handleInputChange}
+                  label="거래상태"
+                >
+                  <MenuItem value="SALE">
+                    <em>판매중</em>
+                  </MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+            <div className="deal-form__detail">
+              <p>상품 상세 내용을 입력해주세요. 상품 상태도 기입해주세요</p>
+              <textarea
+                name="detail"
+                value={detail}
+                onChange={handleInputChange}
+                placeholder="설명"
+              />
+            </div>
           </div>
-        </div>
-        <button type="submit" className="form-submit-btn">
-          완료
-        </button>
-      </form>
-    </SellWrapper>
+          <button type="submit" className="form-submit-btn">
+            완료
+          </button>
+        </form>
+      </SellWrapper>
+    </>
   );
 };
 
